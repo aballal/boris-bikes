0040a12ebe6afe3b89c6b2fba9f54eec4c5dd0eb
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-07 18:10:29 +0100

    Same as previous commit, missed deleting some sections

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,13 +1,3 @@
 class Garage
 
-  attr_reader :broken_bikes_report
-
-  def initialize
-    @broken_bikes_report = Hash.new(0)
-  end
-
-  def receive_broken_bikes_report(docking_station,broken_bikes_count)
-    @broken_bikes_report[docking_station] += broken_bikes_count
-  end
-
 end
